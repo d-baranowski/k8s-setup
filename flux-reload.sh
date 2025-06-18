44a771db60aba9a01a1695783fcf743281a99f98
Daniel Baranowski <daniel.m.baranowski@gmail.com>
2025-06-18 21:11:49 +0200

    n8n secrets rename

--- flux-reload.sh
+++ flux-reload.sh
@@ -1,3 +1,13 @@
 flux reconcile source git flux-system
+flux reconcile kustomization atlas-operator
+flux reconcile kustomization cert-manager
+flux reconcile kustomization external-secrets-operator
+flux reconcile kustomization external-secrets-resources
+flux reconcile kustomization flux-system
+flux reconcile kustomization kubernetes-sigs
+flux reconcile kustomization n8n-user-job
+flux reconcile kustomization nginx-ingress
+flux reconcile kustomization openebs
+flux reconcile kustomization pg-operator
 sleep 3
 flux get kustomizations
