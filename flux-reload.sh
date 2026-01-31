502cfd686bb49305c76d87333ac0cd594b4fb678
Daniel Baranowski <daniel.m.baranowski@gmail.com>
2026-01-31 22:00:43 +0100

    move stuff to default namespace to avoid the need to copy the secrets around

--- flux-reload.sh
+++ flux-reload.sh
@@ -5,10 +5,9 @@ flux reconcile kustomization external-secrets-operator
 flux reconcile kustomization external-secrets-resources
 flux reconcile kustomization flux-system
 flux reconcile kustomization kubernetes-sigs
-flux reconcile kustomization n8n
 flux reconcile kustomization nginx-ingress
 flux reconcile kustomization openebs
-flux reconcile kustomization cloudnative-pg
 flux reconcile kustomization pg-operator
+flux reconcile kustomization n8n
 sleep 3
 flux get kustomizations
